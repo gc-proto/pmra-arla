be96158701cbffe740e1a73aa96092165c9aa1b6
Andrea <andrea.hill@gmail.com>
2022-02-22 12:47:55 -0800

    added test banner (via JS)
    Added test banner

---
+++ js/alpha-banner.js
@@ -0,0 +1,2 @@
+var alphaBanner = document.getElementsByTagName('BODY')[0];
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">This webpage is in alpha</h2><div class="container"><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; This is an experimental page for the Pest Management Regulatory Agency. Contents will change regularly as we engage with the public.</small></div></section >');
